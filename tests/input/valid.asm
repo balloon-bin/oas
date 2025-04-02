0d3881f680353e895681f6bebe65ea273140310c
omicron <omicron.me@protonmail.com>
2025-04-02 11:19:35 +0200

    Update the test input file to contain all AST nodes

--- tests/input/valid.asm
+++ tests/input/valid.asm
@@ -1,5 +1,17 @@
+.section text
+
+; Small valid code snippet that should contain all different AST nodes
+
 _start:
-    mov eax, 555            ; move 555 into eax
+    mov eax, ebx
+    lea eax, [eax + ebx * 4 + 8]
+    lea eax, [eax + 8]
+    lea eax, [eax + ebx * 8]
+    lea eax, [esp - 24]
+    lea eax, [eax + ebx * 4 - 8]
+    lea eax, [_start]
+    mov eax, _start
+    mov eax, 555
     push 0o777
     xor eax, 0xDEADBEEF
     and ecx, 0o770
